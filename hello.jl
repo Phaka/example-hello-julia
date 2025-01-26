589cf73a786d9084d163f830f682482fa0c53974
Werner Strydom <bloudraak@users.noreply.github.com>
2025-01-26 07:23:30 -0800

    Create hello.jl

---
+++ hello.jl
@@ -0,0 +1,7 @@
+#!/usr/bin/env julia
+# Hello World in Julia
+# Julia combines high-level syntax with high performance
+# Functions can be generic, handling multiple types efficiently
+# Unicode symbols can be used in code and mathematics
+
+println("Hello, World!")
